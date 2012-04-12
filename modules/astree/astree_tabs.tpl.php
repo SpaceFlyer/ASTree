cf4fbdd58f74944865ec3476081ba005b6412ed1
Yuqian Li <liyuqian79@gmail.com>
2012-04-12 15:11:02 -0400

    Add template

---
+++ modules/astree/astree_tabs.tpl.php
@@ -0,0 +1,26 @@
+<script>
+	$(function(){
+		$('#astree_tabs').tabs();
+	});
+</script>
+
+<div id="astree_tabs">
+	<ul>
+		<li><a href="#tabs-1">Children</a></li>
+		<li><a href="#tabs-2">Related</a></li>
+		<li><a href="#tabs-3">Comments</a></li>
+	</ul>
+	
+	<div id="tabs-1">
+		<?php echo render($children); ?>
+	</div>
+	
+	<div id="tabs-2">
+		<?php echo render($related); ?>
+	</div>
+	
+	<div id="tabs-3">
+		<?php echo render($comments); ?>
+	</div>
+	
+</div>
