747836206db1dbd5a8309e90362819eb0547b210
Yuqian Li <liyuqian79@gmail.com>
2012-04-24 16:01:41 -0400

    Dialog done for add new child.
    Polish CSS style, make AJAX, then done.

--- modules/astree/astree_tabs.tpl.php
+++ modules/astree/astree_tabs.tpl.php
@@ -17,23 +17,31 @@
 			hide($children['recommend_child_list']);
 			echo render($children); 
 		?>
-		<h2 class="title"><?= t('Add existed node as child') ?></h2>
-		<ul>
-			<li>
-				<form action="?q=add_child/<?= $children['#parent_id'] ?>" method="post">
-					<input type="text" name="child_link_or_nid" id="child_link_or_nid"/>
-					<input type="submit" value="Submit">
-				</form>
-			</li>
-			<?php foreach ($children['recommend_child_list'] as $child) { ?>
-			<li>
-				<?php echo $child['title'] ?>
-				</br>
-				<?php echo $child['action'] ?>
-			</li>
-			<?php } ?>
-		</ul>
-		<h2 class="title child-form"><?php echo t('Add new child'); ?></h2>
+		<div id="add_existed_child_tab">
+			<h2 class="title"><?= t('Add existed node as child') ?></h2>
+			<form action="?q=add_child/<?= $children['#parent_id'] ?>" method="post">
+				<label><?= t('The link or id of the node') ?></label>
+				<input type="text" name="child_link_or_nid" id="child_link_or_nid"/>
+				<input type="submit" value="Submit">
+			</form>
+			<hr/>
+			<ul>
+				<?php foreach ($children['recommend_child_list'] as $child) { ?>
+				<li>
+					<?php echo $child['title'] ?>
+					</br>
+					<?php echo $child['action'] ?>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+		<script>
+			jQuery(document).ready(function(){
+				jQuery('#add_existed_child_tab').dialog({ autoOpen: false });
+			});
+		</script>
+		<a href="javascript:void(0)" onclick="jQuery('#add_existed_child_tab').dialog('open')">Add new child from existed node</a>
+		<h2 class="title child-form"><?php echo t('Create new node as child'); ?></h2>
 		<?php echo render($children['new_child_form']); ?>
 	</div>
 	
