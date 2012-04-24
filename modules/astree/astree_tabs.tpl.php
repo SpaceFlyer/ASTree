77c59bbdfee176d5ecd48dcaa2a02b65c3ae4000
SpaceFlyer <liyuqian79@gmail.com>
2012-04-24 14:19:02 -0400

    Create list/forms to add child.
    Later this can be adapted to related.
    Anyway, UI shall be further polished.

--- modules/astree/astree_tabs.tpl.php
+++ modules/astree/astree_tabs.tpl.php
@@ -12,7 +12,27 @@
 	</ul>
 	
 	<div id="tabs-1">
-		<?php hide($children['new_child_form']); echo render($children); ?>
+		<?php 
+			hide($children['new_child_form']); 
+			hide($children['recommend_child_list']);
+			echo render($children); 
+		?>
+		<h2 class="title"><?= t('Add existed node as child') ?></h2>
+		<ul>
+			<li>
+				<form action="?q=add_child/<?= $children['#parent_id'] ?>" method="post">
+					<input type="text" name="child_link_or_nid" id="child_link_or_nid"/>
+					<input type="submit" value="Submit">
+				</form>
+			</li>
+			<?php foreach ($children['recommend_child_list'] as $child) { ?>
+			<li>
+				<?php echo $child['title'] ?>
+				</br>
+				<?php echo $child['action'] ?>
+			</li>
+			<?php } ?>
+		</ul>
 		<h2 class="title child-form"><?php echo t('Add new child'); ?></h2>
 		<?php echo render($children['new_child_form']); ?>
 	</div>
